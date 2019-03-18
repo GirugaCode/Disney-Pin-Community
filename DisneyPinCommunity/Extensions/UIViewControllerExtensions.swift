9cae67ddf7970d6619cd8854721edf5a1dad6a20
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-03-18 12:57:24 -0700

    [init] Start of project

---
+++ DisneyPinCommunity/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewControllerExtensions.swift
+//  DisneyPinCommunity
+//
+//  Created by Ryan Nguyen on 3/18/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    static func instantiate() -> Self {
+        return self.init(nibName: String(describing: self), bundle: nil)
+    }
+}
